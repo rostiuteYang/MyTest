4ddaef7fb60803a0499297cc0317aa2a81986e47
yrr <test@yrr.com>
2018-11-12 17:24:30 +0800

    no message

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+<template>
+<div>
+  <!-- 50元购买5000点券 -->
+  <div class="goods-items checked" title="50元购5000英雄联盟点卷">
+  <div class="total service">
+    <div>
+      <p class="service_quantity">
+        <span class="service_num">5000</span>
+      </p>
+      <div class="service_gift"></div>
+    </div>
+  </div>
+  <div>
+    <em class="money">50</em>
+    <em class="unit">元</em>
+  </div>
+  <i class="icon-corner-checked"></i>
+  </div>
+
+  <!-- 100元购买10000点券 -->
+  <div class="goods-items checked" title="100元购10000英雄联盟点卷">
+  <div class="total service">
+    <div>
+      <p class="service_quantity">
+        <span class="service_num">5000</span>
+      </p>
+      <div class="service_gift"></div>
+    </div>
+  </div>
+  <div class="relative-to-total">
+    <em class="money">100</em>
+    <em class="unit">元</em>
+  </div>
+  <i class="icon-corner-checked"></i>
+</div>
+
+ <!-- 200元购买20000点券 -->
+
+ <div class="goods-items checked" title="200元购20000英雄联盟点卷">
+  <div class="total service">
+    <div>
+      <p class="service_quantity">
+        <span class="service_num">5000</span>
+      </p>
+      <div class="service_gift"></div>
+    </div>
+  </div>
+  <div class="relative-to-total">
+    <em class="money">100</em>
+    <em class="unit">元</em>
+  </div>
+  <i class="icon-corner-checked"></i>
+</div>
+
+
+ <div class="goods-items" title>
+  <div class="total service">
+    <div>
+      <p class="service_quantity" style="position:relative">
+        <input class="aaa" id="111" placeholder="其他数量">
+      </p>
+      <div class="service_gift"></div>
+    </div>
+  </div>
+  <div class="relative-to-total">
+    <em class="money">100</em>
+    <em class="unit">元</em>
+  </div>
+  <i class="icon-corner-checked"></i>
+</div>
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
